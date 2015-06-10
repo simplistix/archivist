f57ca6f43ab86844a1113688bab0b3c9e7377d3b
Chris Withers <chris@simplistix.co.uk>
2015-06-10 08:46:23 +0100

    boilerplate

---
+++ setup.py
@@ -0,0 +1,51 @@
+# See license.txt for license details.
+# Copyright (c) 2015 Simplistix Ltd
+
+import os, sys
+
+from setuptools import setup, find_packages
+
+base_dir = os.path.dirname(__file__)
+
+setup(
+    name='archivist',
+    version='0.0.dev0',
+    author='Chris Withers',
+    author_email='chris@simplistix.co.uk',
+    license='MIT',
+    description=(
+        "A tool for checking versions of packages used by conda or pip "
+        "are as specified in their requirements files."
+    ),
+    #long_description=open(os.path.join(base_dir,'docs','description.rst')).read(),
+    url='https://github.com/Simplistix/archivist',
+    classifiers=[
+        # 'Development Status :: 5 - Production/Stable',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.3',
+        'Programming Language :: Python :: 3.4',
+    ],
+    packages=find_packages(),
+    zip_safe=False,
+    include_package_data=True,
+    install_requires=[
+        'pyyaml',
+        'dulwich'
+    ],
+    extras_require=dict(
+        test=[
+            'testfixtures',
+            'nose',
+            'nose-cov',
+            ],
+        build=['sphinx', 'pkginfo', 'setuptools-git', 'twine']
+    ),
+    entry_points = {
+        'console_scripts': [
+            'archivist = archivist.main:main',
+    ]}
+)
