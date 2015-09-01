d09e239b667f35922a588609ad99824736103cc1
Chris Withers <chris@simplistix.co.uk>
2015-09-01 10:22:11 +0100

    helper for running stuff in a subprocess

--- archivist/helpers.py
+++ archivist/helpers.py
@@ -1,6 +1,38 @@
 import os
+from subprocess import Popen, PIPE
 
 
 def ensure_dir_exists(directory):
     if not os.path.exists(directory):
         os.makedirs(directory)
+
+
+class CalledProcessError(Exception):
+    """This exception is raised when a process run by check_call() or
+    check_output() returns a non-zero exit status.
+    The exit status will be stored in the returncode attribute;
+    check_output() will also store the output in the output attribute.
+    """
+    def __init__(self, command, returncode, stdout, stderr):
+        self.command = command
+        self.returncode = returncode
+        self.stdout = stdout
+        self.stderr = stderr
+
+    def __str__(self):
+        return (
+            "{command!r} went wrong:\n"
+            "returncode: {returncode}\n\n"
+            "stdout:\n"
+            "{stdout}\n"
+            "stderr:\n"
+            "{stderr}\n".format(**self.__dict__))
+
+
+def run(command, cwd=None):
+    process = Popen(command, stdout=PIPE, stderr=PIPE, cwd=cwd)
+    out, err = process.communicate()
+    if err or process.returncode:
+        raise CalledProcessError(command, process.returncode,
+                                 out, err)
+    return out
---
+++ tests/test_helpers.py
@@ -0,0 +1,61 @@
+import os
+from unittest import TestCase
+import sys
+
+from testfixtures import TempDirectory, compare, ShouldRaise, OutputCapture
+from archivist.helpers import run, CalledProcessError
+
+
+class TestRun(TestCase):
+
+    def run_command(self, code, **kw):
+        with TempDirectory() as dir:
+            self.path = dir.write('test.py', code+'\n')
+            with OutputCapture() as output:
+                result = run([sys.executable, self.path], **kw)
+            output.compare('')
+        return result
+
+    def test_stdout(self):
+        result = self.run_command("import sys; sys.stdout.write('okay')")
+        compare(result, 'okay')
+
+    def test_stderr(self):
+        with ShouldRaise(CalledProcessError) as s:
+            self.run_command("import sys; sys.stderr.write('bad')")
+        compare(str(s.raised), """\
+{command} went wrong:
+returncode: 0
+
+stdout:
+
+stderr:
+bad
+""".format(command=[sys.executable, self.path]))
+
+    def test_returncode(self):
+        with ShouldRaise(CalledProcessError) as s:
+            self.run_command("import sys; sys.exit(13)")
+        compare(str(s.raised), """\
+{command} went wrong:
+returncode: 13
+
+stdout:
+
+stderr:
+
+""".format(command=[sys.executable, self.path]))
+
+    def test_working_directory(self):
+        with TempDirectory() as somewhere:
+            # dance 'cos of weird mac temp directories
+            previous = os.getcwd()
+            os.chdir(somewhere.path)
+            expected = os.getcwd()
+            os.chdir(previous)
+
+            result = self.run_command("import os; print os.getcwd()",
+                                      cwd=somewhere.path)
+            compare(result, expected+'\n')
+
+
